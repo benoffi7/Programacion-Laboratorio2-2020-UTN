f98ddf42004d20ef31e524709da34ae848006a35
Sergio Garguir <37402181+TechMDQLabV@users.noreply.github.com>
2020-09-16 19:58:59 -0300

    Clase 16

---
+++ COM9-Garguir/Clase16/01-estructuras/utilidades.h
@@ -0,0 +1,10 @@
+#ifndef UTILIDADES_H_INCLUDED
+#define UTILIDADES_H_INCLUDED
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int randomRango(int min, int max);
+void replicante(char c, int n);
+
+#endif // UTILIDADES_H_INCLUDED
