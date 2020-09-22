1ee22af9b8e0fcb44efd0180bd9b247260b03d58
Sergio Garguir <37402181+TechMDQLabV@users.noreply.github.com>
2020-09-22 20:32:48 -0300

    Clase 18

---
+++ COM9-Garguir/Clase18/01-estructuras/fila.c
@@ -0,0 +1,53 @@
+#include "fila.h"
+
+void inicFila(Fila* fila){
+    fila->inicio = inicLista2();
+    fila->fin = inicLista2();
+}
+
+void agregar(Fila* fila, stCliente dato){
+    nodo2* nuevo = crearNodo2(dato);
+
+    fila->fin = agregarAlFinal2(fila->fin, nuevo);
+    if(fila->inicio==NULL){
+        fila->inicio = fila->fin;
+    }
+    fila->fin=nuevo;
+}
+
+stCliente primero(Fila* fila){
+    stCliente c;
+    if(fila->inicio){
+        c=verPrimero2(fila->inicio);
+    }
+
+    return c;
+}
+
+stCliente extraer(Fila* fila){
+    stCliente c;
+    if(fila->inicio){
+        c = verPrimero2(fila->inicio);
+        fila->inicio = borrarPrimero2(fila->inicio);
+        if(!fila->inicio){
+            fila->fin = inicLista2();
+        }
+    }
+    return c;
+}
+
+int filaVacia(Fila* fila){
+    int resp=0;
+    if(!fila->inicio){   /// if(fila->inicio == NULL)
+        resp=1;
+    }
+    return resp;
+}
+
+int filaVaciaPro(Fila* fila){
+    return (fila->inicio)?0:1;
+}
+
+void muestraFila(Fila* fila){
+    muestraLista2(fila->inicio);
+}
