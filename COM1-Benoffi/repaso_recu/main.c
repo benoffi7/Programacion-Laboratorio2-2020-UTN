047580cdfa72d1f45185103f0f4d0e8c882cf322
Gonzalo Benoffi <benoffi11@gmail.com>
2020-10-06 11:50:19 -0300

    Repaso de filas

--- COM1-Benoffi/repaso_recu/main.c
+++ COM1-Benoffi/repaso_recu/main.c
@@ -1,34 +1,98 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main()
+//3. Diseñar una aplicación que ofrezca, a través de un menú, la posibilidad de calcular áreas de figuras
+//geométricas específicas. Se deben codificar funciones que calculen las áreas de: círculo, cuadrado, rectángulo,
+// triángulo y trapecio. Las fórmulas pueden buscarse en libros de geometría de nivel secundario (polimodal).
+#include<stdio.h>
+#include<conio.h>
+float triangulo(int base, int altura);
+int cuadrado(int lado);
+void menu () ;
+int main ()
 {
-    cantarCancion(2,0);
+    char seguir;
+    printf("desea calcular un area? (s/n)");
+    fflush(stdin);
+    scanf("%c", &seguir );
+    while(seguir=='s')
+    {
+        menu();
+
+        printf("desea calcular un area? (s/n)");
+        fflush(stdin);
+        scanf("%c", &seguir);
+    }//fin de while
     return 0;
-}
 
-void cantarNaNaNa(int limite, int vuelta)
+}
+float triangulo(int base, int altura)
 {
-    if (vuelta<limite)
+    float resultado ;
+    resultado=(float)(base*altura)/(float)2 ;
+    return resultado;
+}//fin funcion triangulo
+int cuadrado(int lado)
+{
+    int resultado;
+    resultado=lado*lado ;
+
+    return resultado;
+}//fin funcion cuadrado
+
+int rectangulo(int base, int altura)
+{
+    int resultado;
+    resultado= base*altura ;
+    return resultado;
+} //fin funcion rectangulo
+float trapecio (float base_mayor, float base_menor, float altura)
+{
+    float resultado;
+    resultado=((base_mayor+base_menor)*altura)/2;
+    return resultado;
+}//fin funcion trapecio
+void menu ()
+{
+    int opcion;
+    int base_menu, altura_menu, lado_menu, base_rectangulo, altura_rectangulo, base_mayor, base_menor, altura ;
+    float resultado;
+    printf("ingrese 1 para calcular el area de un triangulo \n");
+    printf("ingrese 2 para calcular el area de un cuadrado \n");
+    printf("ingrese 3 para calcular el area de un rectangulo \n");
+    printf("ingrese 4 para calcular el area de un trapecio \n");
+    printf("ingrese 5 para salir del programa");
+    scanf("%d", &opcion);
+    switch (opcion)
     {
-        printf("-Nah-");
-        cantarNaNaNa(limite,vuelta+1);
-    }
-}
+    case 1:
+        printf("ingrese la base y altura del triangulo \n");
+        scanf("%d %d", &base_menu, &altura_menu);
+        resultado=triangulo(base_menu, altura_menu);
+        printf("el area del triangulo es: %f \n", resultado);
+        break;
+    case 2:
+        printf("ingrese un lado del cuadrado \n");
+        scanf("%d", &lado_menu);
+        resultado=cuadrado(lado_menu);
+        printf("el area del cuadrado es : %f \n", resultado);
+        break ;
+    case 3:
+        printf("ingrese base y altura del rectangulo \n");
+        scanf("%d %d", &base_rectangulo, &altura_rectangulo);
+        resultado= rectangulo(base_rectangulo, altura_rectangulo);
+        printf("el area del rectangulo es: %d \n", resultado);
+        break;
+    case 4:
+        printf("ingrese la base mayor , base menor y la altura \n");
+        scanf("%f %f %f", base_mayor, base_menor, altura);
+        resultado=trapecio(base_mayor, base_menor, altura);
+        printf("el area del trapecio es: %f \n");
+        break;
 
-void cantarEstribillo()
-{
-    cantarNaNaNa(9,0);
-    printf("-Hey Jude!-");
+    case 5:
+        printf("gracias por usar el programa \n");
+        break;
+    default:
+        printf("error \n");
+        break ;
 
-}
-
-void cantarCancion(int limite, int vuelta)
-{
-    if (vuelta < limite)
-    {
-        cantarEstribillo();
-        printf("\n");
-        cantarCancion(limite,vuelta+1);
-    }
+    }//fin funcion menu
 }
