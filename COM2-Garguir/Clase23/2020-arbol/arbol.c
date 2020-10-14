e152ed31330db97dd473aca649b0641bafae3227
Sergio Garguir <37402181+TechMDQLabV@users.noreply.github.com>
2020-10-14 14:46:59 -0300

    Arbol

---
+++ COM2-Garguir/Clase23/2020-arbol/arbol.c
@@ -0,0 +1,56 @@
+#include "arbol.h"
+
+nodoArbol* inicArbol(){
+    return NULL;
+}
+
+nodoArbol* crearNodoArbol(int dato){
+    nodoArbol* nuevo = (nodoArbol*) malloc(sizeof(nodoArbol));
+
+    nuevo->dato = dato;
+    nuevo->izq = NULL;
+    nuevo->der = NULL;
+    return nuevo;
+}
+
+nodoArbol* insertar(nodoArbol* arbol, nodoArbol* nuevo){
+    if(!arbol){
+        arbol = nuevo;
+    }else{
+        if(nuevo->dato > arbol->dato){
+            arbol->der = insertar(arbol->der, nuevo);
+        }else{
+            arbol->izq = insertar(arbol->izq, nuevo);
+        }
+    }
+
+    return arbol;
+}
+
+void mostrarNodoArbol(nodoArbol* nodo){
+    printf(" %d -", nodo->dato);
+}
+
+void preOrder(nodoArbol* arbol){
+    if(arbol){
+        mostrarNodoArbol(arbol);
+        preOrder(arbol->izq);
+        preOrder(arbol->der);
+    }
+}
+
+void inOrder(nodoArbol* arbol){
+    if(arbol){
+        inOrder(arbol->izq);
+        mostrarNodoArbol(arbol);
+        inOrder(arbol->der);
+    }
+}
+
+void postOrder(nodoArbol* arbol){
+    if(arbol){
+        postOrder(arbol->izq);
+        postOrder(arbol->der);
+        mostrarNodoArbol(arbol);
+    }
+}
---
+++ COM2-Garguir/Clase23/2020-arbol/arbol.h
@@ -0,0 +1,12 @@
+#ifndef ARBOL_H_INCLUDED
+#define ARBOL_H_INCLUDED
+
+typedef struct nodoArbol{
+    int dato;
+    struct nodoArbol* izq;
+    struct nodoArbol* der;
+}nodoArbol;
+
+
+
+#endif // ARBOL_H_INCLUDED
